1a896b6ff3e9de9a47be2c48878b42e40b4fd192
merryta <merryta251@gmail.com>
2021-06-01 18:02:26 +0000

    print JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/urs/bin/python3
+"""
+contains to_json_string function
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    function return the json repersention of an objecrt(string)
+    """
+    json.dumps(my_obj)
+    return(json.dumps(my_obj))
