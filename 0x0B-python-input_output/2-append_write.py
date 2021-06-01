fa01f901cc692fea4b7321a5876dbeae116085fd
merryta <merryta251@gmail.com>
2021-06-01 23:45:00 +0000

    add text and count

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ contans append_write function
 
 def append_write(filename="", text=""):
     """
-    append a string at the end of text file(UTF8) and  returns number of charcter added
+    append string end of text file(UTF8) & returns number of char added
     """
     with open(filename, "a", encoding="utf-8") as f:
         return(f.write(text))
