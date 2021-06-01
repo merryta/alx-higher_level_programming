838d430c2bc095c97d8694ea526f36d17d51e1ae
merryta <merryta251@gmail.com>
2021-06-01 23:40:47 +0000

    write text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,7 @@ contains fuction write_file
 
 def write_file(filename="", text=""):
     """
-    function  writes a string to text fileie(UTF8) and returns the numbber of the charctere
+    writes string to text fileie(UTF8) & returns the number of the char
     """
     with open(filename, "w", encoding="utf-8") as f:
         return(f.write(text))
