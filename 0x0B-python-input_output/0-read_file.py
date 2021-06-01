f8f3e17a0f91afb856776bcf61c0d6090723b3dc
merryta <merryta251@gmail.com>
2021-06-01 23:30:44 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+model read text file from UTF8 and prints it stdout
+'''
+
+
+def read_file(filename=""):
+    '''
+    reads text file(UTF8)  and print it stdout
+    '''
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
+    
