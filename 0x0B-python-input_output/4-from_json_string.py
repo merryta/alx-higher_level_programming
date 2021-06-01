288443e2cc799526d321e61781a858b2c57a8e5e
merryta <merryta251@gmail.com>
2021-06-01 19:56:39 +0000

    json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+create function from_json_string 
+"""
+
+
+
+import json
+def from_json_string(my_str):
+    """
+    fuction return object(python data structure) reprsented by JSON string
+    """
+    return json.loads(my_str)
