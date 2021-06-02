86a983054a08372c342234d2b2a256397a9505b3
merryta <merryta251@gmail.com>
2021-06-02 07:49:48 +0000

    return an object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,9 +5,10 @@ contains to_json_string function
 
 
 import json
+
+
 def to_json_string(my_obj):
     """
     function return the json repersention of an objecrt(string)
     """
-    json.dumps(my_obj)
     return(json.dumps(my_obj))
