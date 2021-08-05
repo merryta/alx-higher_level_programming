24f76a484b3543c52c6f2166179004e395dfa27a
merryta <merryta251@gmail.com>
2021-08-05 20:23:29 +0000

    print multiple text

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 'use stric';
-let first = 'C is fun';
-let second = 'Python is coo';
-let third = 'javaScript is amazing';
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'JavaScript is amazing';
 console.log(first);
 console.log(second);
 console.log(third);
